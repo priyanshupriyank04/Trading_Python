2ab1eb0b7bef1dcedc9951741fddee81efcc300c
priyanshupriyank04 <133745730+priyanshupriyank04@users.noreply.github.com>
2024-11-13 18:09:49 +0530

    Create Selenium_automation1.py

---
+++ Selenium_automation1.py
@@ -0,0 +1,24 @@
+#This is sample code to be used for just automated web opening of the specified stock on Yfinance
+
+
+
+
+from selenium import webdriver  #does all the web scraping automation 
+from selenium.webdriver.common.by import By   #import By class from selenium module 
+
+
+path = "your_chrome_driver_path_here"   #write the path of the chromer diver into it where you have placed the chrome driver on your system
+service = webdriver.chrome.service.Service(path)    #add the service to automate opening of chrome browser 
+service.start()         #this initiates opening the browser 
+
+ticker = "AAPL"         #write the stock name you want to scrape 
+
+#create empty url to mimic the financial of yfinance
+#add ticker in the placeholder in the provided url structure 
+url = "https://finance.yahoo.com/quote/{}/financials?p={}".format(ticker, ticker)
+
+driver = webdriver.Chrome(service=service)  #use it to open the url 
+driver.get(url)                 #gets the url we created
+driver.implicitly_wait(2)       #use the implicit wait function to ensure that browser is allowed to load even in poor connection conditions 
+
+
